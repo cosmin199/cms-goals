483103042dd38871a5b180039875700a9dce8f83
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2022-02-12 02:21:58 -0800

    auth jwt

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require("express-async-handler")
 const Goal = require("../models/goalModel")
+const User = require("../models/userModel")
 
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find()
+  const goals = await Goal.find({ user: req.user.id })
   res.status(200).json(goals)
 })
 // @desc Set Goal
@@ -18,6 +19,7 @@ const setGoal = asyncHandler(async (req, res) => {
   }
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   })
   res.status(200).json(goal)
 })
@@ -32,6 +34,20 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("goal not found")
   }
 
+  const user = await User.findById(req.user.id)
+
+  // check for user
+  if (!user) {
+    res.status(401)
+    throw new Error("user not found")
+  }
+
+  // make sure the logged user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error("user not authorized")
+  }
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
@@ -43,9 +59,24 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id)
+  // check for goal
   if (!goal) {
     res.status(400)
     throw new Error("goal not found")
+  }
+
+  const user = await User.findById(req.user.id)
+
+  // check for user
+  if (!user) {
+    res.status(401)
+    throw new Error("user not found")
+  }
+
+  // make sure the logged user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error("user not authorized")
   }
   await goal.remove()
   // const deletedGoal = await Goal.findByIdAndRemove(req.params.id, {
